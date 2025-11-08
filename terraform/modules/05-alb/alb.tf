c4d6b3ea9ff1aee29ce3de275b774e9cf12d6a0c
zhenya457 <zheka4573@gmail.com>
2025-11-09 01:27:19 +0200

    delete serificate

--- terraform/modules/05-alb/alb.tf
+++ terraform/modules/05-alb/alb.tf
@@ -28,11 +28,6 @@ resource "aws_lb_target_group" "backend" {
     healthy_threshold   = 2
     unhealthy_threshold = 2
   }
-
-  tags = merge(
-    var.tags,
-    { "Name" = "${var.name}-backend" }
-  )
 }
 
 resource "aws_lb_target_group" "web_ui_react" {
@@ -51,11 +46,6 @@ resource "aws_lb_target_group" "web_ui_react" {
     healthy_threshold   = 2
     unhealthy_threshold = 2
   }
- 
-  tags = merge(
-    var.tags,
-    { "Name" = "${var.name}-ui_react" }
-  )
 }
 
 resource "aws_lb_target_group" "web_ui_angular" {
@@ -70,15 +60,10 @@ resource "aws_lb_target_group" "web_ui_angular" {
     protocol            = "HTTP"
     matcher             = "200"
     interval            = 30
-    timeout             = 5
+    timeout            = 5
     healthy_threshold   = 2
     unhealthy_threshold = 2
   }
- 
-  tags = merge(
-    var.tags,
-    { "Name" = "${var.name}-angular" }
-  )
 }
 
 resource "aws_lb_target_group" "grafana" {
@@ -97,11 +82,6 @@ resource "aws_lb_target_group" "grafana" {
     healthy_threshold   = 2
     unhealthy_threshold = 2
   }
-  
-  tags = merge(
-    var.tags,
-    { "Name" = "${var.name}-grafana" }
-  )
 }
 
 resource "aws_lb_target_group" "prometheus" {
@@ -116,135 +96,20 @@ resource "aws_lb_target_group" "prometheus" {
     protocol            = "HTTP"
     matcher             = "200-399"
     interval            = 30
-    timeout             = 5
+    timeout            = 5
     healthy_threshold   = 2
     unhealthy_threshold = 2
   }
-
-  tags = merge(
-    var.tags,
-    { "Name" = "${var.name}-prometheus" }
-  )
 }
 
+# ✅ Единственный HTTP Listener
 resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.this.arn
   port              = 80
   protocol          = "HTTP"
 
   default_action {
-    type = "redirect"
-
-    redirect {
-      protocol = "HTTPS"
-      port     = "443"
-      status_code = "HTTP_301" # Permanent redirect
-    }
-  }
-}
-
-resource "aws_lb_listener" "https" {
-  load_balancer_arn = aws_lb.this.arn
-  port              = 443
-  protocol          = "HTTPS"
-  certificate_arn   = var.certificate_arn
-
-  default_action {
-    type             = "forward"
-    
-    forward {
-      target_group {
-        arn  = aws_lb_target_group.web_ui_react.arn
-        weight = 50 # 50% to React
-      }
-      target_group {
-        arn  = aws_lb_target_group.web_ui_angular.arn
-        weight = 50 # 50% to Angular
-      }
-      stickiness {
-        enabled  = true
-        duration = 1800
-      }
-    }
-  }
-}
-
-resource "aws_lb_listener_rule" "https_angular_host" {
-  listener_arn = aws_lb_listener.https.arn
-  priority     = 100
-
-  condition {
-    host_header {
-      values = ["angular.${var.domain_name}"]
-    }
-  }
-
-  action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.web_ui_angular.arn
-  }
-}
-
-resource "aws_lb_listener_rule" "https_react_host" {
-  listener_arn = aws_lb_listener.https.arn
-  priority     = 200
-
-  condition {
-    host_header {
-      values = ["react.${var.domain_name}"]
-    }
-  }
-
-  action {
-    type             = "forward"
+    type = "forward"
     target_group_arn = aws_lb_target_group.web_ui_react.arn
-  }
-}
-
-resource "aws_lb_listener_rule" "https_grafana_host" {
-  listener_arn = aws_lb_listener.https.arn
-  priority     = 400
-
-  condition {
-    host_header {
-      values = ["grafana.${var.domain_name}"] # Match hostname
-    }
-  }
-
-  action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.grafana.arn
-  }
-}
-
-resource "aws_lb_listener_rule" "https_prometheus_host" {
-  listener_arn = aws_lb_listener.https.arn
-  priority     = 500
-
-  condition {
-    host_header {
-      values = ["prometheus.${var.domain_name}"] # Match hostname
-    }
-  }
-
-  action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.prometheus.arn
-  }
-}
-
-resource "aws_lb_listener_rule" "https_backend_host" {
-  listener_arn = aws_lb_listener.https.arn
-  priority     = 300
-
-  condition {
-    host_header {
-      values = ["backend.${var.domain_name}"] # Match hostname
-    }
-  }
-
-  action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.backend.arn
   }
 }
