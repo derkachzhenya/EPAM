e3a65c6ad2033ee39497149294c7e4383ed6a279
zhenya457 <zheka4573@gmail.com>
2025-11-10 02:33:40 +0200

    Enable CORS

--- backend/ApiService/Source/Api/Program.cs
+++ backend/ApiService/Source/Api/Program.cs
@@ -3,17 +3,35 @@ using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// ✅ ДОБАВЛЯЕМ ЧТЕНИЕ ENV
 builder.Configuration
     .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
     .AddEnvironmentVariables();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("FrontendCors", policy =>
+    {
+        policy
+            .WithOrigins(
+                "http://3.66.16.191",
+                "http://3.126.116.73",
+                "https://app-alb-584806949.eu-central-1.elb.amazonaws.com"
+            )
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials();
+    });
+});
+
 builder = builder.ConfigureApplicationBuilder();
 
-var app = builder
-    .Build()
-    .ConfigureApplication();
+var app = builder.Build();
 
+app.UseCors("FrontendCors");
+
+app = app.ConfigureApplication();
+
+// Запуск
 try
 {
     Log.Information("Starting host");
